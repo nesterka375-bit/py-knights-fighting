aff4af03ebb94227dc59eb0ec3228820fb38ee43
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-10-26 23:09:24 +0100

    solved py-knights-fighting

--- app/main.py
+++ app/main.py
@@ -6,16 +6,14 @@ def battle(knights_config: dict) -> dict:
     # BATTLE PREPARATIONS:
 
     # lancelot
-    knights_list = []
     for knight in knights_config:
         knights_config[knight] = battle_preparation(knights_config[knight])
-        knights_list.append(knights_config[knight])
 
     # 1 Lancelot vs Mordred:
-    fight_1 = fight(knights_list[0], knights_list[2])
+    fight_1 = fight(knights_config["lancelot"], knights_config["mordred"])
 
     # 2 Arthur vs Red Knight:
-    fight_2 = fight(knights_list[1], knights_list[3])
+    fight_2 = fight(knights_config["arthur"], knights_config["red_knight"])
 
     fight_1.update(fight_2)
     # Return battle results:
--- app/preparations.py
+++ app/preparations.py
@@ -13,17 +13,20 @@ def battle_preparation(knight: dict) -> dict:
     potion = knight.get("potion")
 
     if potion:
-        if "power" in potion.get("effect"):
-            knight["power"] += knight.get("potion").get("effect").get("power")
-
-        if "hp" in potion.get("effect"):
-            knight["hp"] += knight.get("potion").get("effect").get("hp")
-
-        if "protection" in potion.get("effect"):
-            knight["protection"] += (
-                knight.get("potion").
-                get("effect").
-                get("protection")
-            )
+        knight["power"] += (
+            knight.get("potion").
+            get("effect").
+            get("power")
+        ) if "power" in potion.get("effect") else 0
+        knight["hp"] += (
+            knight.get("potion").
+            get("effect").
+            get("hp")
+        ) if "hp" in potion.get("effect") else 0
+        knight["protection"] += (
+            knight.get("potion").
+            get("effect").
+            get("protection")
+        ) if "protection" in potion.get("effect") else 0
 
     return knight
