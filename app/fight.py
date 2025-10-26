6ff64591542f6778315c9c648897f24a824131e7
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-10-26 22:33:21 +0100

    solved py-knights-fighting

---
+++ app/fight.py
@@ -0,0 +1,16 @@
+def fight(knight_1: dict, knight_2: dict) -> dict:
+
+    knight_1["hp"] -= knight_2.get("power") - knight_1.get("protection")
+    knight_2["hp"] -= knight_1["power"] - knight_2["protection"]
+
+    # check if someone fell in battle
+    if knight_1.get("hp") <= 0:
+        knight_1["hp"] = 0
+
+    if knight_2.get("hp") <= 0:
+        knight_2["hp"] = 0
+
+    return {
+        knight_1["name"]: knight_1["hp"],
+        knight_2["name"]: knight_2["hp"],
+    }
---
+++ app/preparations.py
@@ -0,0 +1,33 @@
+# from app.knights import knights
+
+
+def battle_preparation(knight: dict) -> dict:
+
+    # apply armour
+    knight["protection"] = 0
+    for armour in knight.get("armour"):
+        if not armour.get("protection"):
+            knight["protection"] = 0
+        else:
+            knight["protection"] += armour.get("protection")
+
+    # apply weapon
+    knight["power"] += knight.get("weapon").get("power")
+
+    # apply potion if exist
+    potion = knight.get("potion")
+
+    if potion and "power" in potion.get("effect"):
+        knight["power"] += knight.get("potion").get("effect").get("power")
+
+    if potion and "hp" in potion.get("effect"):
+        knight["hp"] += knight.get("potion").get("effect").get("hp")
+
+    if potion and "protection" in potion.get("effect"):
+        knight["protection"] += (
+            knight.get("potion").
+            get("effect").
+            get("protection")
+        )
+
+    return knight
